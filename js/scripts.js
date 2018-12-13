f177d98005b91d05d503b75e12273408d467b275
CareyFrancis <carexfm@gmail.com>
2018-12-13 11:52:33 +0300

    Added if functions to calculate final score

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,27 @@ $(document).ready(function () {
     answer.push($("input:radio[nam=answer4]:checked").val());
     answer.push($("input:radio[nam=answer5]:checked").val());
     answer.push($("input:radio[nam=answer6]:checked").val());
-
+    $(#body1).hide(funtion(event){
+      $("display").html("Your final score is /5");
+    })
+    if (answer[0]==) {
+      score++;
+    }
+    if (answer[1]==) {
+      score++;
+    }
+    if (answer[2]==) {
+      score++;
+    }
+    if (answer[3]==) {
+      score++;
+    }
+    if (answer[4]==){
+      score++;
+    }
+    if (answer[5]==){
+      score++;
+    }
   })
 
 })
